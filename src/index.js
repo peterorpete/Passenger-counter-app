7aac959c95c2b2f7e6bfc2e48e9d319227dae2ad
Peter SImmons <peterorpete@me.com>
2021-06-16 15:51:14 +0100

    counts

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 // document.getElementById("count").innerText = 5
 
-// cmd+k+c
-// let firstBatch = 5
-// let secondBatch = 7
-// let count = firstBatch + secondBatch
+// let count = 5
+// count = count + 1
 // console.log(count)
 
-// 1. Create two variables, myAge and humanDogRatio
-// 2. Multiply the two together and store the result in myDogAge
-// 3. Log myDogAge to the console
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
 
-let myAge = 35;
-let humanDogRatio = 7;
+let bonusPoints = 50;
+console.log(bonusPoints);
 
-let myDogAge = myAge * humanDogRatio;
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
 
-console.log(myDogAge);
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
