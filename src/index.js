a63ce37d2576e1d32edcfcd6313b9e5692ecf0c6
Peter SImmons <peterorpete@me.com>
2021-06-16 15:43:56 +0100

    lesson done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 // document.getElementById("count").innerText = 5
 
-let count = 0;
+// cmd+k+c
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+// console.log(count)
 
-console.log(count);
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge
+// 3. Log myDogAge to the console
 
-// 1. Create a variable, myAge, and set its value to your age
-let myAge = 38;
-// 2. Log the myAge variable to the console
-console.log(myAge);
+let myAge = 35;
+let humanDogRatio = 7;
+
+let myDogAge = myAge * humanDogRatio;
+
+console.log(myDogAge);
