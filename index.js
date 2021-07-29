4a81f8df3210d3cffa0ace7314dccc8299bd03d0
Peter SImmons <peterorpete@me.com>
2021-07-29 15:39:59 +0100

    added a save button

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+let countEl = document.getElementById("count-el");
 let count = 0;
 
 function increment() {
-  let countAmount = document.getElementById("count-el");
-  count++;
-  return (countAmount.innerText = count);
+  count = count + 1;
+  countEl.innerText = count;
+}
+
+// 1. Create a function, save(), which logs out the count when it's called
+
+function save() {
+  console.log(count);
 }
