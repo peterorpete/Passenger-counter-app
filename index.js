780835dbb7aade3766037f8f38acf9a6d8b50643
Pete <peterorpete@me.com>
2021-08-17 16:40:14 +0100

    changed to textContent

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ let countEl = document.getElementById("count-el");
 let count = 0;
 
 function increment() {
-  count += 1;
-  countEl.innerText = count;
+ count += 1;
+ countEl.innerText = count;
 }
 
 function save() {
-  // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-  let previousScore = `${count} -`;
+ // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+ let previousScore = `${count} - `;
 
-  // 3. Render the variable in the saveEl using innerText
-  saveEl.innerHTML += `${previousScore}`;
-  // NB: Make sure to not delete the existing content of the paragraph
-  console.log(count);
+ // 3. Render the variable in the saveEl using innerText
+ saveEl.textContent += `${previousScore}`;
+ // NB: Make sure to not delete the existing content of the paragraph
+ console.log(count);
 }
